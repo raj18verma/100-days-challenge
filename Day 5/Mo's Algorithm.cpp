93b2ca11bdf7e751c25b17af167c39899fc143a8
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-23 04:41:23 +0530

    Create Mo's Algorithm.cpp

---
+++ Day 5/Mo's Algorithm.cpp
@@ -0,0 +1,3 @@
+// Question Link:- https://www.geeksforgeeks.org/mos-algorithm-query-square-root-decomposition-set-1-introduction/
+
+// Refer to this video: https://youtu.be/tuxMRkKuP8Y?si=TboKCcz-N2rX4PLB
