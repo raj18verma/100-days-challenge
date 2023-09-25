a7ef04b4aa87e50ddd2c76ef0e581e1148ddf3ae
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-26 05:15:38 +0530

    Create Subarray sum equals k.cpp

---
+++ Day 8/Subarray sum equals k.cpp
@@ -0,0 +1,30 @@
+// Question Link: https://leetcode.com/problems/subarray-sum-equals-k/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+    int n = arr.size(); // size of the given array.
+    unordered_map<int,int>mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+        
+    }
+};
+
+// Refer this article:- https://takeuforward.org/arrays/count-subarray-sum-equals-k/
