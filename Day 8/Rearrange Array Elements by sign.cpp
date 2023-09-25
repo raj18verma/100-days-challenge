04ba06a244a81880ed42d78bb9bdf752b613f7b7
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-26 05:07:54 +0530

    Create Rearrange Array Elements by sign.cpp

---
+++ Day 8/Rearrange Array Elements by sign.cpp
@@ -0,0 +1,33 @@
+// Question Link:- https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& A) {
+      int n = A.size();
+  
+  // Define array for storing the ans separately.
+  vector<int> ans(n,0);
+  
+  // positive elements start from 0 and negative from 1.
+  int posIndex = 0, negIndex = 1;
+  for(int i = 0;i<n;i++){
+      
+      // Fill negative elements in odd indices and inc by 2.
+      if(A[i]<0){
+          ans[negIndex] = A[i];
+          negIndex+=2;
+      }
+      
+      // Fill positive elements in even indices and inc by 2.
+      else{
+          ans[posIndex] = A[i];
+          posIndex+=2;
+      }
+  }
+  
+  return ans;  
+    }
+};
+
+
+// Refer this article: https://takeuforward.org/arrays/rearrange-array-elements-by-sign/
