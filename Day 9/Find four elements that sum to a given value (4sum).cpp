ad57740ec0316ef2fb28bce160ed3f096aa8e08a
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-27 06:16:12 +0530

    Create Find four elements that sum to a given value (4sum).cpp

---
+++ Day 9/Find four elements that sum to a given value (4sum).cpp
@@ -0,0 +1,50 @@
+// Question Link: https://practice.geeksforgeeks.org/problems/find-all-four-sum-numbers1732/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+
+class Solution{
+    public:
+    // arr[] : int input array of integers
+    // k : the quadruple sum required
+    vector<vector<int> > fourSum(vector<int> &nums, int target) {
+        // Your code goes here
+    int n = nums.size(); 
+    vector<vector<int>> ans;
+
+    sort(nums.begin(), nums.end());
+
+    //calculating the quadruplets:
+    for (int i = 0; i < n; i++) {
+        // avoid the duplicates while moving i:
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int j = i + 1; j < n; j++) {
+            // avoid the duplicates while moving j:
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            // 2 pointers:
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++; l--;
+
+                    //skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
+                }
+                else if (sum < target) k++;
+                else l--;
+            }
+        }
+    }
+
+    return ans;
+    }
+};
+
+
+// Refer this article:- https://takeuforward.org/data-structure/4-sum-find-quads-that-add-up-to-a-target-value/
