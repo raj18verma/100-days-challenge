47a4cc0a2661a8b6b387bb09de6832d82a691bed
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-29 12:42:56 +0530

    Create Print Subarrays with 0 sum.cpp

---
+++ Day 11/Print Subarrays with 0 sum.cpp
@@ -0,0 +1,31 @@
+// Question Link:- https://www.geeksforgeeks.org/print-all-subarrays-with-0-sum/
+
+class Solution{
+public:
+    //Function to count subarrays with sum equal to 0.
+    long long int findSubarray(vector<long long int> &arr, int n ) {
+        //code here
+    map<long long int,int> mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - 0;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+    }
+};
+
+
+// Refer this article: https://takeuforward.org/arrays/count-subarray-sum-equals-k/
