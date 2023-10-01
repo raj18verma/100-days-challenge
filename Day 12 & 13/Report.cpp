09078c9174f2119d3b895111b71ea98c771fb331
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-01 22:59:42 +0530

    Create Report.cpp

---
+++ Day 12 & 13/Report.cpp
@@ -0,0 +1,27 @@
+Basically this was a complete web dev and revision weekend. Where I participated in contests on both days, solved pretty good problems. 
+The list is down. When I created a non-responsive landing page meanwhile I was even brushing up the basics and fundamentals of Javascript.
+
+https://www.hackerrank.com/contests/algorumble-2024-season-september-iii/challenges/sam-is-sick
+
+https://www.hackerrank.com/contests/algorumble-2024-season-september-iii/challenges/index-removal
+
+https://www.hackerrank.com/contests/algorumble-2024-season-september-iii/challenges/book-bank
+
+https://www.hackerrank.com/contests/algorumble-2024-season-september-iii/challenges/fomo-king
+
+https://www.hackerrank.com/contests/algorumble-2024-season-september-iii/challenges/bits-majority
+
+https://www.hackerrank.com/contests/algorumble-2024-season-september-iii/challenges/maximum-unity
+
+https://leetcode.com/contest/biweekly-contest-114/submissions/detail/1063137499/
+
+https://leetcode.com/contest/biweekly-contest-114/submissions/detail/1063137499/
+
+https://leetcode.com/submissions/detail/1064128714/
+
+https://leetcode.com/contest/weekly-contest-365/submissions/detail/1063596082/
+
+Some more fundamentals of JS such as Objects,functions, scopes and IIFE. 
+DOM ongoing.....
+
+https://github.com/raj18verma/Non-Responsive-Landing-Page-1
