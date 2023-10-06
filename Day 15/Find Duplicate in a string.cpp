a296cd3565a6cd5895f957c529029ec5e52506ac
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-06 12:58:47 +0530

    Create Find Duplicate in a string.cpp

---
+++ Day 15/Find Duplicate in a string.cpp
@@ -0,0 +1,18 @@
+// Question Link: https://www.geeksforgeeks.org/print-all-the-duplicates-in-the-input-string/
+
+    unordered_map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        // increase the count of character str[i] by 1
+        count[str[i]]++;
+    }
+    // iterating through the unordered map
+    for (auto it : count) {
+        // if the count of characters is greater than 1 then
+        // duplicate found
+        if (it.second > 1)
+            cout << it.first << ", count = " << it.second
+                 << "\n";
+    }
+}
+
+// T.C:- O(n)  and S.C:- O(n)
