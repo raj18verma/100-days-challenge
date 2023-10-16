68b17b3b637d2884001112bc2b43e599b3d669e5
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-16 22:08:14 +0530

    Create Report.cpp

---
+++ Day 26 & 27/Report.cpp
@@ -0,0 +1,35 @@
+Day 26:
+
+https://leetcode.com/problems/count-good-numbers/submissions/1074913587/
+
+https://leetcode.com/problems/string-to-integer-atoi/submissions/1002814628/
+
+https://leetcode.com/problems/find-all-anagrams-in-a-string/submissions/1074992167/
+
+https://leetcode.com/problems/subsets/submissions/1075192012/
+
+https://leetcode.com/contest/biweekly-contest-115/submissions/detail/1075008673/
+
+https://leetcode.com/contest/biweekly-contest-115/submissions/detail/1075056125/
+
+WEB DEV REPORT:
+
+GSAP Animation completed
+Intro of Locomotive JS
+
+
+
+DAY 27: 
+
+https://leetcode.com/contest/weekly-contest-367/submissions/detail/1075431863/
+
+https://leetcode.com/contest/weekly-contest-367/submissions/detail/1075452470/
+
+https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/submissions/1075630384/
+
+
+WEB DEV REPORT:
+
+Deployed a Product Designer Website on my github made using HTML,CSS & JS. Used GSAP for animating the elements and to improve the UX of this website I used Locomotive JS.
+
+Take a Look:- https://raj18verma.github.io/Product-Designer-Page/
