d408c9a7c84586ef49b52f511850ea7a4710bde3
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-21 20:30:03 +0530

    Create Iterative Preorder.cpp

---
+++ Day 30/Iterative Preorder.cpp
@@ -0,0 +1,31 @@
+// Question Link:- https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+void preOrderTrav(TreeNode * curr, vector < int > & preOrder) {
+  if (curr == NULL)
+    return;
+
+  preOrder.push_back(curr -> val);
+  preOrderTrav(curr -> left, preOrder);
+  preOrderTrav(curr -> right, preOrder);
+}
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preOrder;
+        preOrderTrav(root, preOrder);
+        return preOrder;
+    }
+};
