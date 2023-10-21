748dfa419e8893bd3675978e9ea4b74c31f2731a
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-21 20:32:23 +0530

    Create Iterative PostOrder.cpp

---
+++ Day 30/Iterative PostOrder.cpp
@@ -0,0 +1,30 @@
+// Question Link:- https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+void postOrderTrav(TreeNode * curr, vector < int > & postOrder) {
+  if (curr == NULL)
+    return;
+
+  postOrderTrav(curr -> left, postOrder);
+  postOrderTrav(curr -> right, postOrder);
+  postOrder.push_back(curr -> val);
+}
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>postorder;
+        postOrderTrav(root,postorder);
+        return postorder;
+    }
+};
