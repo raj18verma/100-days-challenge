989cd61af5f22476c53ed3c02dc538d21ff0cde1
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-29 22:39:24 +0530

    Create Find the k or of an array.cpp

---
+++ Day 41/Find the k or of an array.cpp
@@ -0,0 +1,16 @@
+// Question Link:- https://leetcode.com/problems/find-the-k-or-of-an-array/description/
+
+class Solution {
+public:
+    int findKOr(vector<int>& nums, int k) {
+        int sum=0;
+        for(int i=0;i<32;i++){
+            int c=0;
+            for(int val : nums){
+                if(val & (1<<i)) c++;
+            }
+            if(c >= k) sum += pow(2,i);
+        }
+        return sum;   
+    }
+};
