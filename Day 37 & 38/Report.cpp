fc82f127d10baeecbdfbc1f29bf1991ed5d0f26a
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-29 22:27:23 +0530

    Create Report.cpp

---
+++ Day 37 & 38/Report.cpp
@@ -0,0 +1,9 @@
+// No. of questions solved:
+
+https://leetcode.com/problems/binary-subarrays-with-sum/
+
+https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+https://leetcode.com/problems/binary-trees-with-factors/description/
+
+https://leetcode.com/problems/count-number-of-nice-subarrays/
