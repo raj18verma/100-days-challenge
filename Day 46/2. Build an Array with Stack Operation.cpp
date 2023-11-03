a511955e2e9388d3ce2530ec7325690216a6bbc2
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-11-03 23:23:26 +0530

    Create 2. Build an Array with Stack Operation.cpp

---
+++ Day 46/2. Build an Array with Stack Operation.cpp
@@ -0,0 +1,24 @@
+// Question Link:- https://leetcode.com/problems/build-an-array-with-stack-operations/description/
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int stream = 1;
+        vector<string> result;
+        
+        int i = 0;
+        while(i < target.size() && stream <= n) {
+            result.push_back("Push");
+            
+            if(stream == target[i]) {
+                i++;
+            } else {
+                result.push_back("Pop");
+            }
+            stream++;
+        }
+        return result;    
+    }
+};
+
+// yt explanation video:- https://youtu.be/nmNLu1Qz-C0?si=aQwyJxuzZHkGw1rN
