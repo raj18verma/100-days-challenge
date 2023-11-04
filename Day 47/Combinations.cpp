4c3120c6f37ad2ab27f3101d207d0c62602c1441
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-11-04 21:19:08 +0530

    Create Combinations.cpp

---
+++ Day 47/Combinations.cpp
@@ -0,0 +1,26 @@
+// Question Link:- https://leetcode.com/problems/combinations/description/
+
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void help(int val,int k,int n,vector<int>&v){
+        
+        if(v.size()==k){         
+            ans.push_back(v);
+            return;
+        }
+        
+        for(int i=val;i<=n;i++){  // traverse each element from [1,n];
+             v.push_back(i);      // perform operation
+             help(i+1,k,n,v);   // call recursion
+             v.pop_back();         // reverse operation (do backtrack)
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>v;
+        help(1,k,n,v);   
+        return ans;
+    }
+};
+
+// yt refer video:- https://youtu.be/MXt55Mh0JZk?si=UAROOWrdHJvWe7Qa
