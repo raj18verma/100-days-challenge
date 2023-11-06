dd21e6ace2c8682abc59de14692bc73407f4681e
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-11-06 16:14:44 +0530

    Create Seat Reservation Manager.cpp

---
+++ Day 49/Seat Reservation Manager.cpp
@@ -0,0 +1,26 @@
+// Question Link:- https://leetcode.com/problems/seat-reservation-manager/description/?envType=daily-question&envId=2023-11-06
+
+class SeatManager {
+public:
+    set<int> st;
+    SeatManager(int n) {
+        for(int i=1;i<=n;i++) st.insert(i);  
+    }
+    
+    int reserve() {
+        int val = *st.begin(); 
+        st.erase(val);
+        return val;   
+    }
+    
+    void unreserve(int seatNumber) {
+        st.insert(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
