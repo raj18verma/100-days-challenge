9574d5f7e2fe2abb1d73881ee120d6c63bd85fd5
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-11-09 16:55:56 +0530

    Create Count Number of Homogenous Substrings.cpp

---
+++ Day 52/Count Number of Homogenous Substrings.cpp
@@ -0,0 +1,24 @@
+// Question Link:- https://leetcode.com/problems/count-number-of-homogenous-substrings/description/
+
+class Solution {
+public:
+
+int MOD=1e9+7;
+    int countHomogenous(string s) {
+        int n = s.length();
+        int result = 0;
+        int count  = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(i > 0 && s[i] == s[i-1]) {
+                count++;
+            } else {
+                count = 1;
+            }   
+            result = (result + count) % MOD;
+        }
+        return result;    
+    }
+};
+
+// yt refer video:- https://youtu.be/qEJR7ox0mNI?si=M7xqnQR4a6TE9fWr
