a378161320c971eb5b3e45dd2133aabf9cc22452
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-11-19 18:21:50 +0530

    Create Reduction Operations to make the array elements equal.cpp

---
+++ Day 62/Reduction Operations to make the array elements equal.cpp
@@ -0,0 +1,15 @@
+// Question Link:- https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/description/?envType=daily-question&envId=2023-11-19
+
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+        int count = 0;
+        for(int i = n-1; i > 0; i--) {
+            if(nums[i] == nums[i-1]) continue;
+            count += n-i;
+        }
+        return count;       
+    }
+};
