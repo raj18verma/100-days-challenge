a1fcfb3a2e9cde1522f23c6c678d8df8c250af9d
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-12-03 01:17:08 +0530

    Create Count Nodes.cpp

---
+++ Day 75/Count Nodes.cpp
@@ -0,0 +1,42 @@
+// Question Link:- https://www.codingninjas.com/studio/problems/count-nodes-of-linked-list_5884?utm_source=youtube&utm_medium=affiliate&utm_campaign=Codestudio_Linkedlistseries&leftPanelTabValue=SUBMISSION
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+int length(Node *head)
+{
+	//Write your code here
+    int count=0;
+    Node *temp=head;
+    while(temp!=NULL){
+        count++;
+        temp=temp->next;
+    }
+    return count;
+}
