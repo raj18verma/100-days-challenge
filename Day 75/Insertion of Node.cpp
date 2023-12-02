6cbd4873dc8fa74a506ba9a9c53f61dad9a88948
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-12-03 01:13:51 +0530

    Create Insertion of Node.cpp

---
+++ Day 75/Insertion of Node.cpp
@@ -0,0 +1,22 @@
+// Question Link:- https://www.codingninjas.com/studio/problems/insert-node-at-the-beginning_8144739?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+Node* insertAtFirst(Node* list, int newValue) {
+    // Write your code here
+    Node * newnode = new Node(newValue);
+    newnode -> next = list;
+    list = newnode;
+    return list;
+}
