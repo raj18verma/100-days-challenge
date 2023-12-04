ba0277575dcd58f1e6072382e3c003d5a4e8c5ce
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-12-04 22:19:00 +0530

    Create Detect a loop in a linked list.cpp

---
+++ Day 77/Detect a loop in a linked list.cpp
@@ -0,0 +1,44 @@
+// Question Link:- https://www.codingninjas.com/studio/problems/cycle-detection-in-a-singly-linked-list_628974?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+bool detectCycle(Node *head)
+{
+	//	Write your code here
+    Node *slow=head;
+    Node *fast=head;
+
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast) return true;
+    }
+    return false;
+}
