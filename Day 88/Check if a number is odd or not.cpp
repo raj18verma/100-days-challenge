f8208bb16809a56d4be91f6a54c53ab917298ebc
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-12-15 20:48:43 +0530

    Create Check if a number is odd or not.cpp

---
+++ Day 88/Check if a number is odd or not.cpp
@@ -0,0 +1,7 @@
+// Question Link:- https://www.codingninjas.com/studio/problems/odd-even_7993579?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+
+string oddEven(int N){
+    // Write your code here.
+    if((N&1)==0) return "even";
+    else return "odd";
+}
