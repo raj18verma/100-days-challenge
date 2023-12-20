c38086df31f99b9677ff5073796c77a54590b4a3
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-12-20 08:18:09 +0530

    Create Buy Two Chocolates.cpp

---
+++ Day 93/Buy Two Chocolates.cpp
@@ -0,0 +1,12 @@
+// Question Link:- https://leetcode.com/problems/buy-two-chocolates/
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int minCost = prices[0] + prices[1];
+        if (minCost <= money) return money - minCost;
+            else return money;  
+    }
+};
+
